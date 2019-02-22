d8177c1b9442d1b0198d6b7b36da5b85a4d07449
Samuel Isaacson <isaacsas@users.noreply.github.com>
2019-02-21 21:27:23 -0500

    update addodes! kwargs in tests

--- test/test_odes.jl
+++ test/test_odes.jl
@@ -20,7 +20,7 @@ reset_timer!(to)
 
 # get the reaction network
 @timeit to "netgen" rn,initialpop = get_rxnetwork_simple(networkname, speciesf, rxsf; printrxs = false)
-@timeit to "addodes" addodes!(rn; build_symjac=false, build_symfuncs=false)
+@timeit to "addodes" addodes!(rn; build_jac=true, build_symfuncs=false)
 @timeit to "ODEProb" oprob = ODEProblem(rn,convert.(Float64,initialpop),(0.,tf))
 show(to)
 
